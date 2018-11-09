699d1b3bfc3eaae47962da669a23bdbc2c6299c3
IACJ <iacjacj@qq.com>
2018-11-09 08:15:59 +0800

    ediing

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -18,38 +18,42 @@ Page({
 
   //拨打电话
   actiondel: function () {
-    wx.showActionSheet({
-      itemList: ['15013356356', '18366196180'],
-      itemColor: '#31c5ff',
-      success: function (res) {
-        console.log(res.tapIndex)
-        if (res.tapIndex === 0) {
-          wx.makePhoneCall({
-            phoneNumber: '15013356356',
-            success: function () {
-              console.log("拨打电话成功！")
-            },
-            fail: function () {
-              console.log("拨打电话失败！")
-            }
-          })
-        } else
-          if (res.tapIndex === 1) {
-            wx.makePhoneCall({
-              phoneNumber: '18366196180',
-              success: function () {
-                console.log("拨打电话成功！")
-              },
-              fail: function () {
-                console.log("拨打电话失败！")
-              }
-            })
-          }
-      },
-      fail: function (res) {
-        console.log(res.errMsg)
-      }
+    wx.showToast({
+      title: '客服没有上班哦～',
+      icon:'none'
     })
+    // wx.showActionSheet({
+    //   itemList: ['15013356356', '18366196180'],
+    //   itemColor: '#31c5ff',
+    //   success: function (res) {
+    //     console.log(res.tapIndex)
+    //     if (res.tapIndex === 0) {
+    //       wx.makePhoneCall({
+    //         phoneNumber: '15013356356',
+    //         success: function () {
+    //           console.log("拨打电话成功！")
+    //         },
+    //         fail: function () {
+    //           console.log("拨打电话失败！")
+    //         }
+    //       })
+    //     } else
+    //       if (res.tapIndex === 1) {
+    //         wx.makePhoneCall({
+    //           phoneNumber: '18366196180',
+    //           success: function () {
+    //             console.log("拨打电话成功！")
+    //           },
+    //           fail: function () {
+    //             console.log("拨打电话失败！")
+    //           }
+    //         })
+    //       }
+    //   },
+    //   fail: function (res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
   },
 
   getuserinfo: function(e) {
@@ -77,6 +81,12 @@ Page({
           })
         }
       }
+    })
+  },
+  timeRecord: function(e) {
+    wx.showToast({
+      title: '您暂无需要续期的订单!',
+      icon: 'none'
     })
   }
 })
